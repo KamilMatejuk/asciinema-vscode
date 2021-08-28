d2ad4f9bac7577b3942f0f95441a43a668d65eb2
kamil <kamilmatejuk@gmail.com>
2021-08-28 21:08:03 +0200

    Formatting for version 2

---
+++ src/command_fix_timing.ts
@@ -0,0 +1,5 @@
+import * as vscode from 'vscode';
+
+export function commandFixTiming() {
+	vscode.window.showInformationMessage('Hello World from asciinema 1!');
+}
---
+++ src/command_join_files.ts
@@ -0,0 +1,5 @@
+import * as vscode from 'vscode';
+
+export function commandJoinFiles() {
+	vscode.window.showInformationMessage('Hello World from asciinema 2!');
+}
